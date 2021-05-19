5be696b381dbb06e3076f87fd0b141fa9c927af5
Yocto Training <stage@smile.fr>
2021-05-19 18:01:02 +0200

    fichiers pste2 2.0

---
+++ piste2/num.py
@@ -0,0 +1,163 @@
+from tools import *
+import copy
+#---------------------------------------------------------------------
+# count : le compteur de numérotation
+# il s'incrémente lors qu'il y a une nouvelle façon de numérotation
+#---------------------------------------------------------------------
+count = 0
+
+#---------------------------------------------------------------------
+# all_numeroter[] : tableau de numérotation
+# Ce tableau contient toutes les numérotations possibles d'un graphe
+# Ce tableau est utilisé pour éliminer les numérotaions identiques
+#---------------------------------------------------------------------
+all_numeroter = []
+
+#---------------------------------------------------------------------
+# add_numeroter(tabS) : fonction ajoute une possibilité de numérotation
+# dans le tableau all_numeroter
+#---------------------------------------------------------------------
+def add_numeroter(tabS):
+    global all_numeroter
+    _tabS = copy.copy(tabS)
+    if _tabS not in all_numeroter:
+        all_numeroter.append(_tabS)
+
+#---------------------------------------------------------------------
+# debut(tabA, n) : 
+# tabA : tableau des arretes corresponde aux arrêtes des sommets du graphe
+# n : le nombre de sommets
+# 
+# on commence numéroter un graphe avec r = 0 (r est le rang minimal
+# avec une intervale de 0 à n (n: le nombre de sommets dans le graphe))
+# puis on fixe le 1er sommet avec la valeur de a=1 (de 1 à n+1)
+# puis numéroter les restes sommets avec la fonction successeur(r, 0, tabA, n)
+#
+# si count est différente à 0, càd une numérotation du graphe est bien fait
+# mark devient à ture
+# on essayer alors une autre numérotation avec a+1 apres la mise à sero de tabS[] et de visit[]
+# lorsqu'on a fini toutes les numérotation avec toutes les valeurs de a,
+# on renvoie r
+# 
+# mais si la numérotaion du graphe avec r=0 et toutes les valeurs de a, c'est impossible
+# on incrémente r, et on essaye ensuit avec r+1 et toutes les valeurs de a
+# 
+# s'il n'y a pas de numéroration possible pour un graphe, mark est alors tjr False
+# on renvoie donc -1
+#---------------------------------------------------------------------
+def debut(tabA, n):
+    global count
+    mark = False
+    for r in range(1, n):
+        for a in range(1, n+1):
+            tabS[0] = a
+            visit[a] = True
+            successeur(r, 0, tabA, n)
+            if count > 0:
+                mark = True
+            clean_tabS(n) # met en zéro tabS[]
+            clean_visit(n) # met en zéro visit[]
+        if mark:
+            return r
+    return -1
+
+#---------------------------------------------------------------------
+# successeur(r, sommet, tabA, n) : fonction récurssive avec DFS
+# r : le rang minimal définie dans la fonction debut(tabA,n)
+# sommet : 0 au début, change avec i
+# tabA : tableau des arretes corresponde aux arrêtes des sommets du graphe
+# n : le nombre de sommets
+#
+# on parcourt tabA[], s'il y a un arrête entre les sommets avec indices 'sommet' et 'i'
+# alors tabA[sommet][i] est à 1
+#
+# de plus, si tabS[i] est vide, une valeur 'value' qui est de 1 à n+1, n'est pas utilisée
+# ainsi, si la capacité de l'arrête entre les sommets avec indice 'sommet' et 'i'
+# qui est plus petite ou égale à r
+# alors on met la valeur de 'value' au (i+1)ième sommet avec l'indice de 'i'
+# et on met visit[value] à False
+#
+# si toutes les sommets sont numérotés, alors on incrémente 'count'
+# ajoute une numérotation dans le tableau all_numeroter[] avec la fonction add_numeroter(tabS)
+# puis inisialise tabS[i] et visit[value]
+# puis return rien
+# value est ensuite s'incrémente, pour essayer avec une autre valeur de 'value' pour (i+1)ième sommet
+# 
+# si toutes les sommets ne sont pas encore touts numérotés
+# on appele ensuite la fonction récurssive successeur(r, i, tabA, n) refait la même manipulation
+# et initialse tabS[i] et visit[value]
+# 
+# cette fonction est fait avec DFS, qui cherche d'abord toutes les valeur possibles de dernière sommet
+# puis revient à l'arrière
+#
+#---------------------------------------------------------------------
+def successeur(r, sommet, tabA, n):
+    global count
+    for i in range(n):
+        if (tabA[sommet][i] == 1) & (tabS[i] == 0):
+            for value in range(1, n+1):
+            	# vérifier l'utilisation de 'value'
+            	# aussi la capacité de l'arrête est plus petit que r ou tabS[i] est vide
+                if (visit[value] == False) & check_rang(r, i, value, tabA, n):
+                    tabS[i] = value
+                    visit[value] = True
+                    # vérifier si touts les sommets sont numérotés
+                    if check_all_visit(n):
+                        count += 1
+                        show_tabS(n)
+                        # ajoute une numérotation réussite
+                        add_numeroter(tabS)
+                        tabS[i] = 0
+                        visit[value] = False
+                        return
+                    successeur(r, i, tabA, n)
+                    tabS[i] = 0
+                    visit[value] = False
+
+
+if __name__ == '__main__':
+
+	# n : Nombre de sommets
+    n = 5
+
+	#---------------------------------------------------------------------
+	# tabA : Tableau des arrets correspondent à l'arrete entre les sommets
+	# Par exemple, dans la 1ère ligne et la 2e colonne de tabA
+	# c'est 1, c'est à dire que il y a un arrete entre les sommets 1 et 2
+	# Dans la 1ère ligne et la 3e colonne de tabA
+	# c'est 1, il y a un arrete entre les sommets 1 et 3, etc
+	#---------------------------------------------------------------------
+    # graphe aligné
+
+    tabA = [
+        [0,1,0,0,0],
+        [1,0,1,0,0],
+        [0,1,0,1,0],
+        [0,0,1,0,1],
+        [0,0,0,1,0]
+    ]
+
+	#tabA = [
+    #	[0,1,1,0],
+    #	[1,0,1,0],
+    #	[1,1,0,1],
+    #	[0,0,1,0]
+   	#]
+    
+	#tabA = [
+	#    [0,1,1,0,0,0,1,0],
+	#    [1,0,0,1,0,0,0,1],
+	#    [1,0,0,1,1,0,0,0],
+	#    [0,1,1,0,0,1,0,0],
+	#    [0,0,1,0,0,1,1,0],
+	#    [0,0,0,1,1,0,0,1],
+	#    [1,0,0,0,1,0,0,1],
+	#    [0,1,0,0,0,1,1,0]
+    #]
+    init_tabS(n)
+    init_visit(n)
+
+    res = debut(tabA,n)
+    print('\nle rang minimal est : {0}\n'.format(res))
+    print('{0} numerotors'.format(count))
+
---
+++ piste2/tools.py
@@ -0,0 +1,90 @@
+#---------------------------------------------------------------------
+# tabS[n] : tableau de sommets 
+# le 1er élément du tabS indique la numérotation du 1er sommets
+#---------------------------------------------------------------------
+tabS = []
+
+#---------------------------------------------------------------------
+# visit[n+1] : tableau de visit
+# le 1er élément de ce tableau n'est jamais utilisé
+# les éléments suivants correspondent l'utilisation de chiffres
+# Par exemple, le 2e élémentde ce tableau avec une indice 1
+# c'est donc le chiffre 1
+# s'il y a 'false' à cette position, càd le chiffre 1 n'est pas encore utilisé
+# si 'ture' dans cette position, càd le chiffre 1 est utilisé
+#---------------------------------------------------------------------
+visit = []
+
+#---------------------------------------------------------------------
+# init_tabS(n) :
+# créer le tabS[] avec la taille 'n' et la valeur '0'
+#---------------------------------------------------------------------
+def init_tabS(n):
+    for i in range(n):
+        tabS.append(0)
+
+#---------------------------------------------------------------------
+# init_visit(n) :
+# créer visit[] avec la taille 'n' et la valeur 'False'
+#---------------------------------------------------------------------
+def init_visit(n):
+    for i in range(n+1):
+        visit.append(False)
+
+#---------------------------------------------------------------------
+# show_tabS(n) :
+# affiche le tabS[] donc la numérotaion
+#---------------------------------------------------------------------
+def show_tabS(n):
+    for i in range(n):
+        print(tabS[i], end = " ")
+    print("\n--------")
+
+#---------------------------------------------------------------------
+# clean_tabS(n) :
+# initialise le tabS[] avec la valeur '0'
+#---------------------------------------------------------------------
+def clean_tabS(n):
+    for i in range(n):
+        tabS[i] = 0
+
+#---------------------------------------------------------------------
+# clean_visit(n) :
+# initialise le visit[] avec la valeur 'False'
+#---------------------------------------------------------------------
+def clean_visit(n):
+    for i in range(n+1):
+        visit[i] = False
+
+#---------------------------------------------------------------------
+# check_all_visit(n) :
+# vérifier la valeur des éléments de tabS[]
+# si l'un des éléments est à 0, càd il n'est pas numéroté
+# alors la numérotation n'est pas encore fini, renvoie False
+#
+# si touts les éléments ne sont pas à 0, renvoie True
+#---------------------------------------------------------------------
+def check_all_visit(n):
+    for i in range(n):
+        if tabS[i] == 0:
+            return False
+    return True
+
+#---------------------------------------------------------------------
+# check_rang(r, sommet, value, tabA, n) :
+# vérifie s'il y a un arrête entre les sommets avec indice 'sommet' et 'n'
+# ainsi si l'écart d'entre ces sommets(on dit que c'est la capacité de l'arrête)
+# qui est plus grand que r(le rang minimal)
+# et si (i)ième sommet n'est pas vide (tabS[i] == 0)
+# alors on renvoie False
+#
+# si tout les sommets sont vérifié, on renvoie True 
+#---------------------------------------------------------------------
+def check_rang(r, sommet, value, tabA, n):
+    for i in range(n):
+        if tabA[sommet][i] == 1:
+            if (abs(value-tabS[i]) <= r) | (tabS[i] == 0):
+                continue
+            else:
+                return False
+    return True
